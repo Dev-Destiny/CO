619f03053d716d6cf28df814040bfca20b8969f2
Dev-Destiny <adesinadestiny10@gmail.com>
2025-09-02 12:21:32 +0100

    Navbar

--- Pixux/src/Comp/Navbar.tsx
+++ Pixux/src/Comp/Navbar.tsx
@@ -4,20 +4,20 @@ import { Menu, Search } from "lucide-react";
 
 function Navbar() {
 	return (
-		<section className='flex justify-between items-center px-10 py-2 mb-[2rem]'>
+		<section className='flex justify-between items-center px-10 py-2 mb-[2rem] border-b-1 border-b-gray-300 fixed top-0 left-0 right-0 backdrop-blur-[20px] bg-[rgba(255,255,255,0.7)]'>
 			<p className='poppins-bold logo text-gray-900 text-[2rem] font-bold flex-1'>
 				co.
 			</p>
 			<ul className='gap-10 text-gray-500 text-[0.9rem] flex-10 justify-center sm:flex hidden'>
-				<li className='relative'>Home</li>
+				<li className='relative'>Courses</li>
+				<li className='relative'>How it works</li>
+				<li className='relative'>Reviews</li>
 				<li className='relative'>About</li>
-				<li className='relative'>Blog</li>
-				<li className='relative'>Resources</li>
 			</ul>
-			<div className='flex gap-5 items-center'>
+			<div className='flex gap-2 items-center'>
 				{/* <img src="Search-icon.svg" alt="" /> */}
-				<Search size={20} />
-				<Button text={"Sign Up"} type={"type1"} />
+				<Button text={"Sign in"} type={"type2"}/>
+				<Button text={"Get Started"} type={"type1"} />
 				<Menu className='sm:hidden flex' />
 			</div>
 		</section>
